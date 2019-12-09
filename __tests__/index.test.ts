d16fcb030d3ef3ce9d614e7a6644e312546ee584
satoexchange <satoexchange@gmail.com>
2019-12-09 08:48:28 +0100

    Setup Package

---
+++ __tests__/index.test.ts
@@ -0,0 +1,125 @@
+import { SatoJSONRPCClient } from '../lib';
+
+var satoClient = new SatoJSONRPCClient('username', 'API_KEY');
+
+// place buy order in DASH/BTC market
+// satoClient
+//   .order({
+//     action: "buy",
+//     market: "DASH/BTC",
+//     price: 0.001,
+//     amount: 10
+//   })
+//   .then(function(response) {
+//     console.log(response.data);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+// get account orders
+// satoClient
+//   .account_orders({ market: "SATX/BTC" })
+//   .then(function(response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+// get account trade history
+// satoClient
+//   .account_trade_history({
+//     filter: {
+//       market_id: 1,
+//       type: "sell"
+//     },
+//     limit: 5,
+//     sort_dir: "ASC"
+//   })
+//   .then(function(response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+// get account balances
+// satoClient
+//   .balances({
+//     include_zero_balances: false,
+//     coin_id: 1
+//   })
+//   .then(function(response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+// get deposit address
+// satoClient
+// .deposit({
+//   coin: "XRP"
+// })
+// .then(function(response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function(error) {
+//   console.log(error);
+// });
+
+// get exchange currencies
+// satoClient
+// .get_currencies({})
+// .then(function(response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// ["catch"](function(error) {
+//   console.log(error);
+// });
+
+// get market history
+// satoClient
+// .get_market_history({ market_id: 1, limit: 2 })
+// .then(function(response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function(error) {
+//   console.log(error);
+// });
+
+// get market orders
+// satoClient
+// .get_market_orders({ market_id: 1, type: "buy" })
+// .then(function(response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function(error) {
+//   console.log(error);
+// });
+
+// get markets
+// satoClient
+// .get_markets({ action: "search", search_q: "XRP" })
+// .then(function(response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function(error) {
+//   console.log(error);
+// });
+
+// place buy order in DASH/BTC market
+satoClient
+  .order({
+    action: 'buy',
+    market: 'DASH/BTC',
+    price: 0.001,
+    amount: 0.1,
+  })
+  .then(function(response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import { sha256 } from 'js-sha256';
+import * as jsonrpc from 'jsonrpc-lite';
+
+export class SatoJSONRPCClient {
+  public apiUrl: string = 'http://satoex.local/api/v2/';
+  private username: string;
+  private id: number;
+  private apiKey: string;
+  constructor(username: string, apiKey: string) {
+    this.username = username;
+    this.apiKey = apiKey;
+    this.id = 0;
+  }
+  public now() {
+    const timestamp = Math.floor(new Date().getTime() / 1000);
+    return timestamp;
+  }
+  // predefined methods
+  public account_orders(params: any) {
+    this.id++;
+    return this.request(this.id, 'account_orders', params);
+  }
+  public account_trade_history(params: any) {
+    this.id++;
+    return this.request(this.id, 'account_trade_history', params);
+  }
+  public balances(params: any) {
+    this.id++;
+    return this.request(this.id, 'balances', params);
+  }
+  public deposit(params: any) {
+    this.id++;
+    return this.request(this.id, 'deposit', params);
+  }
+  public order(params: any) {
+    this.id++;
+    return this.request(this.id, 'order', params);
+  }
+  public get_currencies(params: any) {
+    this.id++;
+    return this.request(this.id, 'get_currencies', params);
+  }
+  public get_market_history(params: any) {
+    this.id++;
+    return this.request(this.id, 'get_market_history', params);
+  }
+  public get_market_orders(params: any) {
+    this.id++;
+    return this.request(this.id, 'get_market_orders', params);
+  }
+  public get_markets(params: any) {
+    this.id++;
+    return this.request(this.id, 'get_markets', params);
+  }
+  private auth(method: string, timestamp: number) {
+    const toAuth: string = this.username + this.apiKey + method + timestamp.toString();
+    return sha256(toAuth);
+  }
+  private request(id: number, method: string, params: any): Promise<any> {
+    if (id < 1) {
+      id = 1;
+    }
+    params.username = this.username;
+    params.timestamp = this.now();
+    params.auth = this.auth(method, params.timestamp);
+    const body = jsonrpc.request(id, method, params);
+    return axios({
+      data: body,
+      headers: { 'Content-type': 'application/json' },
+      method: 'post',
+      url: this.apiUrl,
+    });
+  }
+}
